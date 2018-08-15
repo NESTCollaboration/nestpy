25b1e0e0fd34a10d237147a8848dbbe297ca3a15
Christopher Tunnell <ctunnell@nikhef.nl>
2018-08-15 09:34:05 -0500

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ requirements = [x.split('=')[0] for x in requirements]
 
 setup(
     name='nestpy',
-    version='0.2.0',
+    version='0.2.1',
     author='Christopher Tunnell',
     author_email='tunnell@rice.edu',
     description='Python bindings for the NEST noble element simulations',
