fe070f5c21ffde639c80bdd38fe3b5fad6a8db02
Chris Tunnell <2406627+tunnell@users.noreply.github.com>
2018-08-15 09:26:38 +0200

    cmake --version

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -7,6 +7,7 @@ yum install -y atlas-devel
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     "${PYBIN}/pip" install -r /io/requirements_dev.txt
+    "${PYBIN}/cmake --version"
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
