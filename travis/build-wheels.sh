38269e1966dcbf75937a412c8899ec963a061f4b
Chris Tunnell <2406627+tunnell@users.noreply.github.com>
2018-08-15 08:50:48 +0200

    Update build-wheels.sh

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -3,6 +3,7 @@ set -e -x
 
 # Install a system package required by our library
 yum install -y atlas-devel
+yum install -y cmake  # Needed for pybind11
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
