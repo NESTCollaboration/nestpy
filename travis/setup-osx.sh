e4726d698d9906e18aa95b954e03c9e79e8e1df8
Chris Tunnell <2406627+tunnell@users.noreply.github.com>
2018-08-16 12:00:39 +0200

    Create setup-osx.sh

---
+++ travis/setup-osx.sh
@@ -0,0 +1,11 @@
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
+
+# Install
+chmod +x miniconda.sh
+./miniconda.sh -b -p $HOME/miniconda
+    
+# Setup miniconda and environment
+export PATH=$HOME/miniconda/bin:$PATH
+conda config --set always_yes yes --set changeps1 no
+conda create -q -n nestpy python=${PYTHON}
+source activate nestpy
