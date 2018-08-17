bbfb10c3ec822c19a0e9baf531f3be65d5332a25
Christopher Tunnell <ctunnell@nikhef.nl>
2018-08-17 09:25:21 -0500

    Cleanup tests

--- tests/core_nest_tests.py
+++ tests/core_nest_tests.py
@@ -1,177 +1,146 @@
 import unittest
-from nestpy.nestpy import VDetector, NESTcalc, YieldResult, QuantaResult, NESTresult, INTERACTION_TYPE
+import nestpy
+
+#from nestpy import nestpy.VDetector, NESTcalc, nestpy.YieldResult, QuantaResult, NESTresult, nestpy.INTERACTION_TYPE
+
+class ConstructorTest(unittest.TestCase):
+    """Test constructors
+
+    These are used in the setup of later tests.  Therefore, seperate test
+    here.
+    """
+    def test_vdetector_constructor(self):
+        detector = nestpy.VDetector()
+        assert detector is not None
+        assert isinstance(detector, nestpy.VDetector)
+
+    def test_vdetector_initialization(self):
+        detector = nestpy.VDetector()
+        detector.Initialization()
+        assert detector is not None
+        assert isinstance(detector, nestpy.VDetector)
+
+    def test_nestcalc_constructor(self):
+        nestcalc = nestpy.NESTcalc()
+        assert nestcalc is not None
+        assert isinstance(nestcalc, nestpy.nestpy.NESTcalc)
+
+    def test_nestcalc_constructor_vdetect(self):
+        detector = nestpy.VDetector()
+        detector.Initialization()
+        nestcalc = nestpy.NESTcalc(detector)
+        assert nestcalc is not None
+        assert isinstance(nestcalc, nestpy.nestpy.NESTcalc)
+
+    def test_intteraction_type_constructor(self):
+        it = nestpy.INTERACTION_TYPE(0)
+        assert it is not None
+        assert str(it) != ""
+        assert isinstance(it, nestpy.INTERACTION_TYPE)
+
+
+class VDetectorTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.detector = nestpy.VDetector()
+        cls.detector.Initialization()
+
+    def test_fit_s1(self):
+        self.detector.FitS1(1.0, 2.0, 3.0)
+
+    def test_fit_ef(self):
+        self.detector.FitEF(1.0, 2.0, 3.0)
+        
+    def test_fit_s2(self):
+        self.detector.FitS2(1.0, 2.0)
+
+    def test_fit_tba(self):
+        self.detector.FitTBA(1.0, 2.0, 3.0)
+
+
+
 class NESTcalcTest(unittest.TestCase):
 
-    def test_vdetector_const(self):
-        detec = VDetector()
-        detec.Initialization()
-
-    def test_vdetector_fit_s1(self):
-        detec = VDetector()
-        detec.Initialization()
-        detec.FitS1(1.0, 2.0, 3.0)
-
-    def test_vdetector_fit_ef(self):
-        detec = VDetector()
-        detec.Initialization()
-        detec.FitEF(1.0, 2.0, 3.0)
-
-    def test_vdetector_fit_s2(self):
-        detec = VDetector()
-        detec.Initialization()
-        detec.FitS2(1.0, 2.0)
-
-    def test_vdetector_fit_tba(self):
-        detec = VDetector()
-        detec.Initialization()
-        detec.FitTBA(1.0, 2.0, 3.0)
-
-    def test_nestcalc_const(self):
-        nest = NESTcalc()
-
-    def test_nestcalc_const_vdetect(self):
-        detec = VDetector()
-        detec.Initialization()
-        nest = NESTcalc(detec)
+    @classmethod
+    def setUpClass(cls):
+        cls.detector = nestpy.VDetector()
+        cls.detector.Initialization()
+        cls.it = nestpy.INTERACTION_TYPE(0)
+        cls.nestcalc = nestpy.NESTcalc(cls.detector)
 
     def test_nestcalc_binom_fluct(self):
-        detec = VDetector()
-        detec.Initialization()
-        nest = NESTcalc(detec)
-        nest.BinomFluct(1, 20.)
+        binom = self.nestcalc.BinomFluct(1, 20.)
+        assert binom > 0
+        assert isinstance(binom, int)
+
+    def test_interaction_type_constructor(self):
+        for i in range(5):
+            it = nestpy.nestpy.INTERACTION_TYPE(i)
 
     def test_nestcalc_full_calculation(self):
-        detec = VDetector()
-        detec.Initialization()
-        nest = NESTcalc(detec)
-        int_type = INTERACTION_TYPE(0)
-        result = nest.FullCalculation(int_type, 1., 2., 3., 4, 5, [1, 1])
-
-    def test_nestcalc_photon_time(self):
-        detec = VDetector()
-        detec.Initialization()
-        nest = NESTcalc(detec)
-        int_type = INTERACTION_TYPE(0)
-        result = nest.FullCalculation(int_type, 1., 2., 3., 4, 5, [1, 1])
-        nest.AddPhotonTransportTime(result.photon_times, 1.0, 2.0, 3.0)
-
-    def test_nestcalc_add_photon_transport_time(self):
-        detec = VDetector()
-        detec.Initialization()
-        nest = NESTcalc(detec)
-        int_type = INTERACTION_TYPE(0)
-        result = nest.FullCalculation(int_type, 1., 2., 3., 4, 5, [1, 1])
-        nest.AddPhotonTransportTime(result.photon_times, 1.0, 2.0, 3.0)
-
+        result = self.nestcalc.FullCalculation(self.it, 1., 2., 3., 4, 5, [1, 1])
+        assert isinstance(result, nestpy.NESTresult)
+        
     def test_nestcalc_get_photon_times(self):
-        detec = VDetector()
-        detec.Initialization()
-        nest = NESTcalc(detec)
-        int_type = INTERACTION_TYPE(0)
-        result = nest.FullCalculation(int_type, 1., 2., 3., 4, 5, [1, 1])
-        nest.GetPhotonTimes(int_type, 10, 10, 10., 10.)
+        self.nestcalc.GetPhotonTimes(self.it, 10, 10, 10., 10.)
 
     def test_nestcalc_get_yields(self):
-        detec = VDetector()
-        detec.Initialization()
-        nest = NESTcalc(detec)
-        int_type = INTERACTION_TYPE(0)
-        result = nest.FullCalculation(int_type, 1., 2., 3., 4, 5, [1, 1])
-        yields = nest.GetYields(int_type, 10., 10., 10., 10., 10., [1, 1])
-
-    def test_nestcalc_get_quanta(self):
-        detec = VDetector()
-        detec.Initialization()
-        nest = NESTcalc(detec)
-        int_type = INTERACTION_TYPE(0)
-        result = nest.FullCalculation(int_type, 1., 2., 3., 4, 5, [1, 1])
-        yields = nest.GetYields(int_type, 10., 10., 10., 10., 10., [1, 1])
-        quanta = nest.GetQuanta(yields, 10.)
-
-    def test_nestcalc_get_s1(self):
-        detec = VDetector()
-        detec.Initialization()
-        nest = NESTcalc(detec)
-        int_type = INTERACTION_TYPE(0)
-        result = nest.FullCalculation(int_type, 1., 2., 3., 4, 5, [1, 1])
-        result = nest.FullCalculation(int_type, 1., 2., 3., 4, 5, [1, 1])
-        yields = nest.GetYields(int_type, 10., 10., 10., 10., 10., [1, 1])
-        quanta = nest.GetQuanta(yields, 10.)
-        nest.GetS1(quanta, 0, 1, 10., 10., int_type, 100, 10., 10., 0, False, [0,1,2], [0.,1.,2.])
+        yields = self.nestcalc.GetYields(self.it, 10., 10., 10., 10., 10., [1, 1])
 
     def test_nestcalc_get_spike(self):
-        detec = VDetector()
-        detec.Initialization()
-        nest = NESTcalc(detec)
-        int_type = INTERACTION_TYPE(0)
-        result = nest.FullCalculation(int_type, 1., 2., 3., 4, 5, [1, 1])
-        nest.GetSpike(10, 10., 20., 30., 10., 10., [0, 1, 2])
-
+        self.nestcalc.GetSpike(10, 10., 20., 30., 10., 10., [0, 1, 2])
 
     def test_nestcalc_calculate_g2(self):
-        detec = VDetector()
-        detec.Initialization()
-        nest = NESTcalc(detec)
-        int_type = INTERACTION_TYPE(0)
-        result = nest.FullCalculation(int_type, 1., 2., 3., 4, 5, [1, 1])
-        nest.CalculateG2(True)
+        assert self.nestcalc.CalculateG2(True)[3] > 10
 
     def test_nestcalc_set_drift_velocity(self):
-        detec = VDetector()
-        detec.Initialization()
-        nest = NESTcalc(detec)
-        int_type = INTERACTION_TYPE(0)
-        result = nest.FullCalculation(int_type, 1., 2., 3., 4, 5, [1, 1])
-        nest.SetDriftVelocity(190, 10, 10)
+        self.nestcalc.SetDriftVelocity(190, 10, 10)
 
     def test_nestcalc_set_drift_velocity_mag_boltz(self):
-        detec = VDetector()
-        detec.Initialization()
-        nest = NESTcalc(detec)
-        int_type = INTERACTION_TYPE(0)
-        result = nest.FullCalculation(int_type, 1., 2., 3., 4, 5, [1, 1])
-        nest.SetDriftVelocity_MagBoltz(10, 10)
+        self.nestcalc.SetDriftVelocity_MagBoltz(10, 10)
 
     def test_nestcalc_set_drift_velocity_non_uniform(self):
-        detec = VDetector()
-        detec.Initialization()
-        nest = NESTcalc(detec)
-        int_type = INTERACTION_TYPE(0)
-        result = nest.FullCalculation(int_type, 1., 2., 3., 4, 5, [1, 1])
-        nest.SetDriftVelocity_NonUniform(190, 10, 10, 10)
+        self.nestcalc.SetDriftVelocity_NonUniform(190, 10, 10, 10)
 
     def test_nestcalc_set_denisty(self):
-        detec = VDetector()
-        detec.Initialization()
-        nest = NESTcalc(detec)
-        int_type = INTERACTION_TYPE(0)
-        result = nest.FullCalculation(int_type, 1., 2., 3., 4, 5, [1, 1])
-        nest.SetDensity(190, 10)
-
+        self.nestcalc.SetDensity(190, 10)
 
     def test_nestcalc_photon_energy(self):
-        detec = VDetector()
-        detec.Initialization()
-        nest = NESTcalc(detec)
-        int_type = INTERACTION_TYPE(0)
-        result = nest.FullCalculation(int_type, 1., 2., 3., 4, 5, [1, 1])
-        nest.PhotonEnergy(True, True, 190)
+        self.nestcalc.PhotonEnergy(True, True, 190)
 
     def test_nestcalc_calc_electron_LET(self):
-        detec = VDetector()
-        detec.Initialization()
-        nest = NESTcalc(detec)
-        int_type = INTERACTION_TYPE(0)
-        result = nest.FullCalculation(int_type, 1., 2., 3., 4, 5, [1, 1])
-        nest.CalcElectronLET(100)
+        self.nestcalc.CalcElectronLET(100)
 
     def test_nest_calc_get_detector(self):
-        detec = VDetector()
-        detec.Initialization()
-        nest = NESTcalc(detec)
-        int_type = INTERACTION_TYPE(0)
-        nest.GetDetector()
+        self.nestcalc.GetDetector()
 
+
+class NESTcalcFullCalculationTest(unittest.TestCase):
+        
+    @classmethod
+    def setUpClass(cls):
+        cls.detector = nestpy.VDetector()
+        cls.detector.Initialization()
+        cls.it = nestpy.INTERACTION_TYPE(0)
+        cls.nestcalc = nestpy.NESTcalc(cls.detector)
+        cls.result = cls.nestcalc.FullCalculation(cls.it, 1., 2., 3., 4, 5, [1, 1])
+
+    def test_nestcalc_photon_time(self):
+        self.nestcalc.AddPhotonTransportTime(self.result.photon_times, 1.0, 2.0, 3.0)
+
+    def test_nestcalc_add_photon_transport_time(self):
+        self.nestcalc.AddPhotonTransportTime(self.result.photon_times, 1.0, 2.0, 3.0)
+
+    def test_nestcalc_get_quanta(self):
+        self.nestcalc.GetQuanta(self.result.yields, 10.)
+        
+    def test_nestcalc_get_s1(self):
+        self.nestcalc.GetS1(self.result.quanta, 0, 1, 10., 10., self.it, 100, 10., 10., 0, False, [0,1,2], [0.,1.,2.])
+            
+
+
+        
 
 if __name__ == "__main__":
     unittest.main()
