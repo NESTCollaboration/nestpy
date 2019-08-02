fda4cdc2118e93d8e1920ad6d52c958d0f3536d1
Christopher Tunnell <ctunnell@nikhef.nl>
2019-08-02 05:28:40 -0500

    Sync with 2.0.1

--- src/nestpy/TestSpectra.hh
+++ src/nestpy/TestSpectra.hh
@@ -1,10 +1,4 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
  * File:   TestSpectra.hh
  * Author: brodsky3
  *
@@ -33,7 +27,7 @@
 #define V_WIMP 220.
 #define V_ESCAPE 544.
 
-#define NUMBINS_MAX 200
+#define NUMBINS_MAX 1000
 
 class TestSpectra {
  public:
