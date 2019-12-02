51b521d24154b7c7955ffbb1b25365fa5933a11c
Sophia Andaloro <sja5@rice.edu>
2019-12-02 09:57:06 -0600

    Update main.py

--- nest_local_flask/main.py
+++ nest_local_flask/main.py
@@ -1,16 +1,25 @@
-import io
-import logging 
-import os
-import collections
-from flask import Response, Flask, send_file, request
-from benchmark_plots import makeplots
+'''
+main.py
 
+This is the function for launching flask app which will display benchmark plots. 
+Calls on: 
+- benchmark_plots.py: Holds functions to calculate yields, make plots, and save them into a dictionary.
+- io, collections for storing the dictionary of objects locally.
+- flask: Flask makes apps; 
+send_file allows us to work with saved image dictionary in Flask; 
+request processes arguments in url format.
+'''
+import io
+import collections
+from flask import Flask, send_file, request
+from benchmark_plots import makeplots
 #  TODO: 
 # In IMAGE_OBJECTS, check version of nestpy?  Or generate file upon request?
 
+# Runs makeplots from benchmark_plots.py, stores all plots in IMAGE_OBJECTS
 IMAGE_OBJECTS = collections.defaultdict(io.BytesIO)
+makeplots(IMAGE_OBJECTS) 
 
-makeplots(IMAGE_OBJECTS)
 app = Flask(__name__)
 
 @app.route('/get_image')
@@ -25,7 +34,7 @@ def get_image():
 @app.route('/')
 def hello():
     message = "Welcome to your very first Flask app of nestpy! At the end of this URL Type in:"
-    typecommand = " /get_image?interaction=<type of interaction>&yieldtype=<either LY (light yield) or QY (charge yield)"
+    typecommand = " /get_image?interaction='type of interaction'&yieldtype='either LY (light yield) or QY (charge yield)' "
     return(message + typecommand)
 
 if __name__ == '__main__':
