b8bd75b23e5c8eaf9d03f21f8df5c1733e094bec
Christopher Tunnell <ctunnell@nikhef.nl>
2020-06-22 15:23:52 -0500

    Bump version: 1.2.0 → 1.2.1

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ requirements = open('requirements.txt').read().splitlines()
 
 setup(
     name='nestpy',
-    version='1.2.0',
+    version='1.2.1',
     author='Christopher Tunnell',
     author_email='tunnell@rice.edu',
     description='Python bindings for the NEST noble element simulations',
--- src/nestpy/__init__.py
+++ src/nestpy/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 __nest_version__ = '2.1.0beta'
 
 from .nestpy import *
