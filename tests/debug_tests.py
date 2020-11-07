81aac3800728556eaf556e4a6060ae97519a6fb7
sophiaandaloro <sja5@rice.edu>
2020-11-07 11:20:20 -0600

    Fix nestpy tests for v1.3.0

---
+++ tests/debug_tests.py
@@ -0,0 +1,200 @@
+import unittest
+import nestpy
+
+
+class ConstructorTest(unittest.TestCase):
+    """Test constructors
+
+    These are used in the setup of later tests.  Therefore, seperate test
+    here.
+    """
+
+    def test_vdetector_constructor(self):
+        detector = nestpy.VDetector()
+        assert detector is not None
+        assert isinstance(detector, nestpy.VDetector)
+
+    def test_vdetector_initialization(self):
+        detector = nestpy.VDetector()
+        detector.Initialization()
+        assert detector is not None
+        assert isinstance(detector, nestpy.VDetector)
+
+    def test_xenon_example_constructor(self):
+        detector = nestpy.DetectorExample_XENON10()
+        assert detector is not None
+        assert isinstance(detector, nestpy.DetectorExample_XENON10)
+
+    def test_nestcalc_constructor_vdetect(self):
+        detector = nestpy.VDetector()
+        detector.Initialization()
+        nestcalc = nestpy.NESTcalc(detector)
+        assert nestcalc is not None
+        assert isinstance(nestcalc, nestpy.nestpy.NESTcalc)
+
+    def test_intteraction_type_constructor(self):
+        it = nestpy.INTERACTION_TYPE(0)
+        assert it is not None
+        assert str(it) != ""
+        assert isinstance(it, nestpy.INTERACTION_TYPE)
+
+
+#class VDetectorTest(unittest.TestCase):
+#
+#    @classmethod
+#    def setUpClass(cls):
+#        cls.detector = nestpy.VDetector()
+#        cls.detector.Initialization()
+#
+#    def test_fit_s1(self):
+#        self.detector.FitS1(1.0, 2.0, 3.0)
+#
+#    def test_fit_ef(self):
+#        self.detector.FitEF(1.0, 2.0, 3.0)
+#
+#    def test_fit_s2(self):
+#        self.detector.FitS2(1.0, 2.0)
+#
+#    def test_fit_tba(self):
+#        self.detector.FitTBA(1.0, 2.0, 3.0)
+
+
+class NESTcalcTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.detector = nestpy.VDetector()
+        cls.detector.Initialization()
+        cls.it = nestpy.INTERACTION_TYPE(0)
+        cls.nestcalc = nestpy.NESTcalc(cls.detector)
+
+        cls.nuisance = cls.nestcalc.default_NuisParam
+        cls.free = cls.nestcalc.default_FreeParam
+
+    def test_nestcalc_binom_fluct(self):
+        binom = self.nestcalc.BinomFluct(1, 20.)
+        assert binom > 0
+
+    def test_interaction_type_constructor(self):
+        for i in range(5):
+            it = nestpy.nestpy.INTERACTION_TYPE(i)
+
+    def test_nestcalc_full_calculation(self):
+        result = self.nestcalc.FullCalculation(self.it, 1., 2., 3., 4, 5,
+                                               self.nuisance,
+                                               self.free,
+                                               False)
+        assert isinstance(result, nestpy.NESTresult)
+
+    def test_nestcalc_get_photon_times(self):
+        self.nestcalc.GetPhotonTimes(self.it, 10, 10, 10., 10.)
+
+    def test_nestcalc_get_yields(self):
+        yields = self.nestcalc.GetYields(
+            self.it, 10., 10., 10., 10., 10., self.nuisance)
+
+    def test_nestcalc_get_yields_defaults(self):
+        yields = self.nestcalc.GetYields(nestpy.INTERACTION_TYPE(0),
+                                         10)
+                                         
+    def test_nestcalc_get_yields_named(self):
+        yields = self.nestcalc.GetYields(nestpy.INTERACTION_TYPE(0),
+                                         energy=10)
+
+    def test_nestcalc_get_spike(self):
+        self.nestcalc.GetSpike(10, 10., 20., 30., 10., 10., [0, 1, 2])
+
+    def test_nestcalc_calculate_g2(self):
+        assert self.nestcalc.CalculateG2(True)[3] > 10
+
+    def test_nestcalc_set_drift_velocity(self):
+        self.nestcalc.SetDriftVelocity(190, 10, 10)
+
+    def test_nestcalc_set_drift_velocity_non_uniform(self):
+        self.nestcalc.SetDriftVelocity_NonUniform(190, 10, 10, 10)
+
+    def test_nestcalc_set_denisty(self):
+        self.nestcalc.SetDensity(190, 10)
+
+    def test_nestcalc_photon_energy(self):
+        self.nestcalc.PhotonEnergy(True, True, 190)
+
+    def test_nestcalc_calc_electron_LET(self):
+        self.nestcalc.CalcElectronLET(100)
+
+    def test_nest_calc_get_detector(self):
+        self.nestcalc.GetDetector()
+
+
+class NESTcalcFullCalculationTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.detector = nestpy.VDetector()
+        cls.detector.Initialization()
+        cls.it = nestpy.INTERACTION_TYPE(0)
+
+        cls.nestcalc = nestpy.NESTcalc(cls.detector)
+
+        cls.nuisance = cls.nestcalc.default_NuisParam
+        cls.free = cls.nestcalc.default_FreeParam
+
+        cls.nestcalc = nestpy.NESTcalc(cls.detector)
+        cls.result = cls.nestcalc.FullCalculation(
+            cls.it, 10., 3., 100., 131, 56, 
+            cls.nuisance,
+            cls.free,
+            True)
+
+        cls.position = [2,3,4]
+
+#    def test_nestcalc_add_photon_transport_time(self):
+#        print(self.result.photon_times)
+#        self.nestcalc.AddPhotonTransportTime(
+#            self.result.photon_times, 1.0, 2.0, 3.0)
+
+    def test_nestcalc_get_quanta(self):
+        self.nestcalc.GetQuanta(self.result.yields, 10., self.free)
+        
+    def test_nestcalc_get_quanta_defaults(self):
+        self.nestcalc.GetQuanta(self.result.yields)
+
+#    def test_nestcalc_get_s1(self):
+#        self.nestcalc.GetS1(self.result.quanta, 
+#                            self.position,
+#                            self.position,
+#                            10., 10.,
+#                            self.it, 100, 10., 10.,
+#                            False, False,
+#                            [0, 1, 2],
+#                            [0, 1, 2])
+
+#class execNESTTest(unittest.TestCase):
+#
+#    def test_execNEST_random_pos(self):
+#        detector = nestpy.DetectorExample_XENON10()
+#        #  test with -1 for fObs and seed (1)
+#        nestpy.execNEST(detector, 10, 'NR', 100., 120., 10., "0., 0., 0.", "120.", -1., 1, True, 1.0)
+#
+#    def test_execNEST_pos(self):
+#        detector = nestpy.DetectorExample_XENON10()
+#        #  test with actual position [0.,0.,0.] and seed(1)
+#        nestpy.execNEST(detector, 10, 'NR', 100., 120., 10., "0., 0., 10.", "120.", 1., 1, True, 1.0) 
+#
+#    def test_execNEST_pos_random_seed(self):
+#        detector = nestpy.DetectorExample_XENON10()
+#	#  test with actual position [0.,0.,0.] and randomSeed
+#        nestpy.execNEST(detector, 10, 'NR', 100., 120., 10., "0., 0., 10.", "120.", 1, 1, True, 1.0)
+#
+#    def test_execNEST_random_z(self):
+#        detector = nestpy.DetectorExample_XENON10()
+#	#  test with actual position [0.,0.,0.] and randomSeed
+#        nestpy.execNEST(detector, 10, 'NR', 100., 120., 10., "0., 0., -1", "120.", 1, 1, True, 1.0)
+#
+#    def test_execNEST_random_xy(self):
+#        detector = nestpy.DetectorExample_XENON10()
+#	#  test with actual position [0.,0.,0.] and randomSeed
+#        nestpy.execNEST(detector, 10, 'NR', 100., 120., 10., "-999, -999, 10.", "120", 1, 1, True, 1.0)
+
+if __name__ == "__main__":
+    unittest.main()
