3d2de7c5690d370aec0b0143644695ff4512c0ba
sophiaandaloro <sja5@rice.edu>
2020-11-19 16:43:42 -0600

    Bump version: 1.4.0beta → 1.4.0

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ requirements = open('requirements.txt').read().splitlines()
 
 setup(
     name='nestpy',
-    version='1.4.0beta',
+    version='1.4.0',
     author='Christopher Tunnell',
     author_email='tunnell@rice.edu',
     description='Python bindings for the NEST noble element simulations',
