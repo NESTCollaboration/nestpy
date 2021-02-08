beb319f5f5fc6347290a815b3b0e6390683dea26
sophiaandaloro <sja5@rice.edu>
2021-02-08 12:01:45 -0600

    add nestpy docker image

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 set -e -x
 
-# Install a system package required by our library
-yum install -y atlas-devel wget
-
-# Have to manually build and install CMake since libc old but
-# at least 2.8.12 is required for pybind11.
-cd /root
-#wget --no-check-certificate https://github.com/Kitware/CMake/archive/v2.8.12.tar.gz # SSL too new on this server for old wget
-wget http://astroparticle.rice.edu/v2.8.12.tar.gz
-tar xfz v2.8.12.tar.gz
-cd CMake-2.8.12
-./bootstrap > quiet_bootstrap
-make  > quiet_make
-make install > quiet_make_install
-export PATH=$PATH:/usr/local/bin
-
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
