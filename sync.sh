3952e009998865292c058f526a8c7409668d45b8
Greg Rischbieter <grischbieter@albany.edu>
2021-08-09 18:09:38 -0400

    Undid changes to sync.sh

--- sync.sh
+++ sync.sh
@@ -1,19 +1,18 @@
 #  This script is used for syncing to the NEST main repository
 #  to ensure that the bindings bind to the same version of NEST
 
-#export VERSION=`python -c "import nestpy;print(nestpy.__nest_version__)"`
-#echo $VERSION 
+export VERSION=`python -c "import nestpy;print(nestpy.__nest_version__)"`
+echo $VERSION 
 
-#git clone https://github.com/NESTCollaboration/nest.git nest_source
-#cd nest_source
-#git fetch --all --tags --prune
-#git checkout tags/v${VERSION} -b test
-#cd ..
+git clone https://github.com/NESTCollaboration/nest.git nest_source
+cd nest_source
+git fetch --all --tags --prune
+git checkout tags/v${VERSION} -b test
+cd ..
 
 cd src/nestpy
-for filename in *.{hpp,cpp,hh}; do
-    export REPO_FILE=`find ../../../nest/ -name ${filename}`
-    echo ${filename}
+for filename in *.{cpp,hh}; do
+    export REPO_FILE=`find ../../nest_source/ -name ${filename}`
     if [ ! -z "$REPO_FILE" -a "$REPO_FILE" != " " ]; then
         cp $REPO_FILE $filename
     fi
