5e15a817d5d9cc7d85edda4223d828eddb98f16d
Sophia Farrell <sja5@rice.edu>
2022-02-03 16:43:42 -0600

    add newer python versions to setup

--- setup.py
+++ setup.py
@@ -102,6 +102,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: C++',
         'Intended Audience :: Science/Research',
         'Programming Language :: Python :: Implementation :: CPython',
