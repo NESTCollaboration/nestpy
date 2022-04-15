b7699626d0a1229d4d3e4ce7a955556678b2ab43
Joran Angevaare <jorana@nikhef.nl>
2022-04-15 09:41:31 +0200

    Bump version: 1.5.8 → 1.5.9

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ requirements = open('requirements.txt').read().splitlines()
 
 setup(
     name='nestpy_test2',
-    version='1.5.8',
+    version='1.5.9',
     author='Sophia Farrell',
     author_email='sja5@rice.edu',
     description='Python bindings for the NEST noble element simulations',
--- src/nestpy/__init__.py
+++ src/nestpy/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.5.8'
+__version__ = '1.5.9'
 __nest_version__ = '2.3.6'
 
 from .nestpy import *
